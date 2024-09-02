87bc85ec4189e8ead06004ee706490c51da8490f
ngrilli <nicolo.grilli@bristol.ac.uk>
2024-09-02 23:59:23 +0100

    phase fields object

---
+++ phase_fields.py
@@ -0,0 +1,10 @@
+# Nicolò Grilli
+# Università di Bristol
+# 2 Settembre 2024
+
+# Phase fields eta_i representing the EBSD data
+
+class PhaseFields:
+	
+	def __init__(self,file_name):
+		self.file_name = file_name
