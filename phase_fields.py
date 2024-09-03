c3b19a428dc39e784d1a7f4fdf3f89dfb262b298
ngrilli <nicolo.grilli@bristol.ac.uk>
2024-09-03 11:13:28 +0100

    rotation matrix in phase_fields object

--- phase_fields.py
+++ phase_fields.py
@@ -4,7 +4,34 @@
 
 # Phase fields eta_i representing the EBSD data
 
+import numpy as np
+
 class PhaseFields:
 	
-	def __init__(self,file_name):
+	def __init__(self,file_name,EBSD_data,longitude_sections=1,latitude_sections=1):
 		self.file_name = file_name
+		self.EBSD_data = EBSD_data # an EBSD object
+		# number of nodes along the x and y direction in a structured mesh
+		self.Nx = max(self.EBSD_data.Nx_odd,self.EBSD_data.Nx_even) + 1
+		self.Ny = self.EBSD_data.Ny + 1
+		# the number of sections in which the unit sphere is split along the longitude and latitude
+		self.longitude_sections = longitude_sections
+		self.latitude_sections = latitude_sections
+	
+	# calculate section of the sphere based on Euler angles
+	def sphere_section(self,phi1,Phi,phi2):
+		return 1.0
+		
+	# calculate ZXZ rotation matrix, angles are in radians
+	def rotation_matrix():
+		R = np.zeros(shape=(3,3))
+		R[0,0] = np.cos(phi1) * np.cos(phi2) - np.sin(phi1) * np.sin(phi2) * np.cos(Phi)
+		R[0,1] = - np.cos(phi1) * np.sin(phi2) - np.sin(phi1) * np.cos(phi2) * np.cos(Phi)
+		R[0,2] = np.sin(phi1) * np.sin(Phi)
+		R[1,0] = np.sin(phi1) * np.cos(phi2) + np.cos(phi1) * np.sin(phi2) * np.cos(Phi)
+		R[1,1] = - np.sin(phi1) * np.sin(phi2) + np.cos(phi1) * np.cos(phi2) * np.cos(Phi)
+		R[1,2] = - np.cos(phi1) * np.sin(Phi)
+		R[2,0] = np.sin(phi2) * np.sin(Phi)
+		R[2,1] = np.cos(phi2) * np.sin(Phi)
+		R[2,2] = np.cos(Phi)
+		return R
